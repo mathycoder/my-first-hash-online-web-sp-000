aaf278e63342a2333ab70135ded456015f0d8cca
Adam S <adam.sarli@gmail.com>
2019-03-14 12:38:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,6 @@ def shipping_manifest
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  the_manifest["oil paintings"]
 end
 
 def retrieval
@@ -25,7 +24,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  the_manifest["oil paintings"]
 end
 
 def adding
